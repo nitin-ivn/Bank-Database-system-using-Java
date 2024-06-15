9d866ae271d53490336ca84705c7bc4559551106
nitin-ivn <ivnsainitin@gmail.com>
2024-06-15 20:35:17 +0530

    Added Panels in Fund Transfer

--- Views/Componenets/LoansPanel.java
+++ Views/Componenets/LoansPanel.java
@@ -8,10 +8,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class LoansPanel extends JPanel{
+    String[] loans = {"Home Loans","Vehicle Loans","Personal Loans"};
+
     private final Font labelfont = new Font("sansserif", Font.BOLD,10);
     Color bgcolor = new Color(71,75,75);
+    Font titlefont = new Font("sansserif",Font.BOLD,36);
 
     JLabel title = new JLabel("Loans");
+
+    JPanel HomeLoansPanel = new JPanel();
+    JPanel VehicleLoansPanel = new JPanel();
+    JPanel PersonalLoansPanel = new JPanel();
+    JPanel PaymentPanel = new JPanel();
+
+    MyComboBox comboBox = new MyComboBox(loans);
+    MyButton showbutton = new MyButton("Show");
 
     MyButton newloanbutton = new MyButton("Apply");
 
@@ -22,6 +33,36 @@ public class LoansPanel extends JPanel{
         title.setFont(new Font("sansserif",Font.BOLD,38));
         add(title,"pos 340 10");
 
+        JLabel homepaneltitle = new JLabel("Home Loans");
+        homepaneltitle.setFont(titlefont);
+        HomeLoansPanel.setBackground(Color.WHITE);
+        HomeLoansPanel.setLayout(new MigLayout("fill"));
+        HomeLoansPanel.add(homepaneltitle,"pos 10 10");
+        add(HomeLoansPanel,"width 90%,height 40%,pos 40 150");
+
+        JLabel vehicletitle = new JLabel("Vehicle Loans");
+        vehicletitle.setFont(titlefont);
+        VehicleLoansPanel.setBackground(Color.WHITE);
+        VehicleLoansPanel.setLayout(new MigLayout("fill"));
+        VehicleLoansPanel.add(vehicletitle,"pos 10 10");
+        add(VehicleLoansPanel,"width 90%,height 40%,pos 40 150");
+
+        JLabel personalltitle = new JLabel("Personal Loans");
+        personalltitle.setFont(titlefont);
+        PersonalLoansPanel.setBackground(Color.RED);
+        PersonalLoansPanel.setLayout(new MigLayout("fill"));
+        PersonalLoansPanel.add(personalltitle,"pos 10 10");
+        add(PersonalLoansPanel,"width 90%,height 40%,pos 40 150");
+
+        PaymentPanel.setBackground(Color.BLACK);
+        PaymentPanel.setLayout(new MigLayout("fill"));
+        add(PaymentPanel,"width 90%,height 30%,pos 40 450");
+        PaymentPanel.setVisible(false);
+
+        HomeLoansPanel.setVisible(false);
+        VehicleLoansPanel.setVisible(false);
+        PersonalLoansPanel.setVisible(false);
+
         newloanbutton.setFont(new Font("sansserif",Font.BOLD,18));
         add(newloanbutton,"width 100,height 40,pos 10 10");
         newloanbutton.addActionListener(new ActionListener() {
@@ -31,7 +72,32 @@ public class LoansPanel extends JPanel{
             }
         });
 
+        comboBox.setHint("Select Loan");
+        comboBox.setBackground(new Color(222, 223, 223));
+        comboBox.setSelectedIndex(-1);
+        comboBox.setFocusable(false);
+        add(comboBox,"width 250,height 42,pos 190 90");
 
+        showbutton.setFont(new Font("sansserif",Font.BOLD,18));
+        add(showbutton,"width 100,height 40,pos 450 90");
+        showbutton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(comboBox.getSelectedIndex() == 0){
+                    HomeLoansPanel.setVisible(true);
+                    VehicleLoansPanel.setVisible(false);
+                    PersonalLoansPanel.setVisible(false);
+                }else if(comboBox.getSelectedIndex() == 1){
+                    HomeLoansPanel.setVisible(false);
+                    VehicleLoansPanel.setVisible(true);
+                    PersonalLoansPanel.setVisible(false);
+                }else if(comboBox.getSelectedIndex() == 2){
+                    HomeLoansPanel.setVisible(false);
+                    VehicleLoansPanel.setVisible(false);
+                    PersonalLoansPanel.setVisible(true);
+                }
+            }
+        });
     }
 
 
